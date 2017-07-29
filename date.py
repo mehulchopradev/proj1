e44e0bee7503a75bcde16a03ed150071d25c5294
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-07-29 16:20:10 +0530

    date module

---
+++ date.py
@@ -0,0 +1,1 @@
+# write a function to return the date in a particular format
--- hello.py
+++ hello.py
@@ -1,3 +1,6 @@
+# import the module date.py
 print 'Good Morning To All'
 
 print 'Good afternoon to all'
+
+# call the function from date.py and print the date
