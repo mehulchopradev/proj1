acfa988c1bbe97a7ff0ba6539096cf57cdc0a7d3
Mayuresh Shilotri <mayuresh.shilotri@gmail.com>
2017-07-29 17:25:58 +0530

    student module complete

--- hello.py
+++ hello.py
@@ -8,3 +8,5 @@ print 'Good evening to all'
 # call the function from date.py and print the date
 
 # create student object and call the method
+
+# create one more student object and call the method
